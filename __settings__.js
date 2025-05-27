05dbe4be1ba6c17c21c6d79c650fd1696299be92
Lynkxi <lynkxira@gmail.com>
2025-05-27 18:20:34 +0300

    1

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "484733.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 6079415, 6079416, 6079417, 6079519 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
